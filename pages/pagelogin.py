9a67a4da36e37abe1dfd9311be0466e6295b9453
herbi <herbysaliandy@gmail.com>
2023-03-26 23:06:45 +0700

    test line 118 komen

--- pages/pagelogin.py
+++ pages/pagelogin.py
@@ -6,8 +6,6 @@ from selenium.webdriver.common.actions import interaction
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.pointer_input import PointerInput
 from selenium.webdriver.common.action_chains import ActionChains
-from appium import TouchAction
-#TouchAction touch = new TouchAction(self.driver);
 
 
 class login:
@@ -55,13 +53,6 @@ class login:
             el7 = self.driver.find_element(by=AppiumBy.XPATH, value="//android.widget.Button[@text='Lanjut']")
             el7.click()
             self.driver.implicitly_wait(15)
-            actions = ActionChains(self.driver)
-            actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
-            actions.w3c_actions.pointer_action.move_to_location(816, 1620)
-            actions.w3c_actions.pointer_action.pointer_down()
-            actions.w3c_actions.pointer_action.move_to_location(808, 1728)
-            actions.w3c_actions.pointer_action.release()
-            actions.perform()
             el9 = self.driver.find_element(by=AppiumBy.ID, value="com.idntimes.idntimes:id/ivSettings")
             el9.click()
             self.driver.implicitly_wait(10)
